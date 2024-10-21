54c0ddb2803fd1c3feeae871f2504b337c4eca0d
Alejandro <aletomasperren@gmail.com>
2024-10-21 12:56:42 -0300

    Install dependencies

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db'
 import router from './routes/index'
 
